cddfaf3ee24d62c7aa1a926d564e170054aa73a4
Thanh-679 <duongthanh13004@gmail.com>
2026-09-13 23:30:57 +0700

    Refactor FIFO and UART Tx testbench by removing read_request
    Removed read_request signal and associated connections.

--- simulation/tb_FIFO_and_uart_Tx.v
+++ simulation/tb_FIFO_and_uart_Tx.v
@@ -9,9 +9,7 @@ module tb_FIFO_and_uart_Tx(
     reg CLK_read;
     reg reset_write;
     reg reset_read;
-
     reg write_request;
-    reg read_request;
     reg [`data-1:0]w_data;
 
     wire write_full;
@@ -20,7 +18,6 @@ module tb_FIFO_and_uart_Tx(
     //
 
 //Tx
-wire data_ack;
 wire  data_enable;
 //Rx
 wire error;
@@ -28,9 +25,9 @@ wire end_of_frame;
 wire [`data-1:0] flipflop_for_peripheral;
 top_module_FIFO_Uart_Tx test_FIFO_after_connecting_to_uart_Tx(.CLK_write(CLK_write),.
     CLK_read(CLK_read),.reset_write(reset_write),.reset_read(reset_read),.write_request
-    (write_request),.read_request(read_request),.w_data(w_data),.write_full(write_full)
-    ,.FIFO_nearly_full(FIFO_nearly_full),.FIFO_nearly_empty(FIFO_nearly_empty),.
-    data_ack(data_ack),.data_enable(data_enable),.error(error),.end_of_frame(end_of_frame)
+    (write_request),.w_data(w_data),.write_full(write_full)
+    ,.FIFO_nearly_full(FIFO_nearly_full),.FIFO_nearly_empty(FIFO_nearly_empty)
+    ,.data_enable(data_enable),.error(error),.end_of_frame(end_of_frame)
     ,.flipflop_for_peripheral(flipflop_for_peripheral));
     
         
@@ -50,14 +47,12 @@ initial begin
 reset_write=1'b0;
 reset_read=1'b0;
 write_request=0;
-read_request=0;
 w_data=0;
 #50;
 reset_write=1;
 reset_read=1;
 #53;
 write_request=1'b1;
-read_request=1'b1;
 w_data=8'd89;
 #20;
 w_data=8'd90;
