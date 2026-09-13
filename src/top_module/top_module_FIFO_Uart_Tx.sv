844104aa9fd1a881a19068f00f67f64295287c58
Thanh-679 <duongthanh13004@gmail.com>
2026-09-13 23:28:55 +0700

    Update top_module_FIFO_Uart_Tx.sv

--- src/top_module/top_module_FIFO_Uart_Tx.sv
+++ src/top_module/top_module_FIFO_Uart_Tx.sv
@@ -8,7 +8,6 @@ input logic CLK_write,
     input logic reset_read,
 
     input logic write_request,
-    input logic read_request,
     input logic [`data-1:0]w_data,
 
     output logic write_full,
@@ -17,7 +16,6 @@ input logic CLK_write,
     //
 
 //Tx
-output logic data_ack,
 output logic data_enable,
 //Rx
 output logic error,
@@ -26,12 +24,12 @@ output logic [`data-1:0] flipflop_for_peripheral
        );
     logic [`data-1:0] r_data;
     logic read_empty;
-    
+    logic data_ack;
     logic data_busy;
-
+logic read_request;
 FIFO_asy_topmodule DUT_topmodule_from_FIFO(.CLK_write(CLK_write),.CLK_read(CLK_read),.
 reset_write(reset_write),.reset_read(reset_read),.write_request(write_request),.
-read_request(read_request),.w_data(w_data),.write_full(write_full),.r_data(r_data)//fixed here/
+read_request(data_ack)/*fixed here*/,.w_data(w_data),.write_full(write_full),.r_data(r_data)//fixed here/
 ,.read_empty(read_empty)/*fixed here*/,.FIFO_nearly_full(FIFO_nearly_full),.FIFO_nearly_empty(FIFO_nearly_empty));
 
 top_module_Rx_Tx DUT_topmodule_from_UART_Tx(.internal_clk(CLK_read)/*fixed here*/,.reset_read(reset_read)
